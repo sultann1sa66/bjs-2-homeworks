78b18cd635450b26425674668ed12a638e443662
Sultan Abdulazizov <sultann1.sa66@gmail.com>
2023-09-06 14:48:05 +0300

    1. task.js changed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,20 +2,24 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = b**2-4*a*c
-  let b2 = b - (b * 2);
+  let bNegative = b - (b * 2);
   if(d === 0){
-    let x = b2 / (2 * a);
+    let x = bNegative / (2 * a);
     arr.push(x);
   }
   if (d > 0) {
-  let x = (b2 - Math.sqrt(d)) / (2 * a);
+  let x = (bNegative + Math.sqrt(d)) / (2 * a);
   arr.push(x);
-  let x1 = (b2 + Math.sqrt(d)) / (2 * a);
+  let x1 = (bNegative - Math.sqrt(d)) / (2 * a);
   arr.push(x1);
   }
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let percentPerMounth = percent / (100 * 12);
+  let payment = (amount - contribution) * (percentPerMounth / (1 - (1 + percentPerMounth) ** (countMonths - (countMonths * 2))));
+  let num = payment * countMonths;
+  let result = +num.toFixed(2);
+  return result;
 }
