5c7d09873353dd8e25bfe86ff1ca43402c07515a
Sultan Abdulazizov <sultann1.sa66@gmail.com>
2023-09-06 10:03:09 +0300

    1. first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2-4*a*c
+  let b2 = b - (b * 2);
+  if(d === 0){
+    let x = b2 / (2 * a);
+    arr.push(x);
+  }
+  if (d > 0) {
+  let x = (b2 - Math.sqrt(d)) / (2 * a);
+  arr.push(x);
+  let x1 = (b2 + Math.sqrt(d)) / (2 * a);
+  arr.push(x1);
+  }
   return arr;
 }
 
