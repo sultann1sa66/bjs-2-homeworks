75f570377c912695867a2282717f995cb03c94b4
Sultan Abdulazizov <sultann1.sa66@gmail.com>
2023-09-21 20:16:40 +0300

    task done

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
+  if(arr1.length === arr2.length && arr1.every((elem, index) => elem === arr2[index])){
+
+        
+    return true ;
+    } else {
+        return false;
+    }
   
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+    if(users.filter(user => user.gender === gender).length !== 0) {
+        return  users.filter(user => user.gender === gender).reduce((acc, user) => acc + user.age, 0) / users.filter(user => user.gender === gender).length
+               }
+           return 0;
 }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,35 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
+
+let student1 = new Student('Ann', 'Female', 20);
+let student2 = new Student('Mary', 'Female', 25);
+let student3 = new Student('Max', 'male', 30);
+
+
 
 Student.prototype.setSubject = function (subjectName) {
-  
-}
+    this.subject = subjectName;
+  }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if('marks' in this){
+        this.marks.push(...marksToAdd)
+    }   
 }
 
 Student.prototype.getAverage = function () {
-  
+    if('marks' in this && this.marks.length > 0){
+        return this.marks.reduce((acc, mark, index, arr) => acc + mark / arr.length, 0);
+    }
+  return 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
 }
